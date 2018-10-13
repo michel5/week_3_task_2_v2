8666d0a6d6be2893b6ea729daa55bb0ef66ef79d
micheldaniel <micheldaniel2@hotmail.com>
2018-10-14 00:01:41 +0200

    f
    f

---
+++ app/src/main/java/com/example/michel_desktop/week_3_task_2_v2/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.michel_desktop.week_3_task_2_v2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button vloButton;
+    Button DMCIButton;
+    Button roosterButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        vloButton = findViewById(R.id.vlo_id);
+        vloButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WebVieuwController.class);
+                intent.putExtra("url", "https://vlo.informatica.hva.nl/");
+                startActivity(intent);
+            }
+        });
+
+        DMCIButton = findViewById(R.id.dcmi_id);
+        DMCIButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WebVieuwController.class);
+                intent.putExtra("url", "http://www.hva.nl/faculteit/fdmci/onderwijs/mci-onderwijs.html");
+                startActivity(intent);
+            }
+        });
+
+        roosterButton = findViewById(R.id.rooster_id);
+        roosterButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WebVieuwController.class);
+                intent.putExtra("url", "https://rooster.hva.nl/");
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/michel_desktop/week_3_task_2_v2/WebVieuwController.java
@@ -0,0 +1,44 @@
+package com.example.michel_desktop.week_3_task_2_v2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+
+public class WebVieuwController extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.web_vieuw_layout);
+
+        //krijg de back button
+        button = findViewById(R.id.back_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent INTENT = new Intent(WebVieuwController.this, MainActivity.class);
+                startActivity(INTENT);
+            }
+        });
+
+        //krijg intent
+        Intent intent = getIntent();
+
+        //web vieuw
+        WebView myWebView = (WebView) findViewById(R.id.web_vieuw_id);
+        System.out.println("testje");
+        Log.i("url", "blablabalabala "+intent.getStringExtra("url"));
+       // myWebView.loadUrl(intent.getStringExtra("url"));
+        myWebView.loadUrl("https://vlo.informatica.hva.nl/");
+
+    }
+}
